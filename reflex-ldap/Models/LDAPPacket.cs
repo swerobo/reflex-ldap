45732ee8970ce4938a8ce6f76bf6eaa8ef53574b
Robert Johansson <robert.johansson@ohb-sweden.se>
2023-03-28 19:19:08 +0100

    Inital commit

---
+++ reflex-ldap/Models/LDAPPacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reflex_ldap.Models
+{
+    public class LDAPPacket
+    {
+        private List<LDAPPacket> packets;
+        public string Name { get; set; }
+
+        public LDAPPacket(string Name)
+        {
+            packets = new List<LDAPPacket>();
+        }
+
+        public List<LDAPPacket> GetPackets()
+        {
+            return packets;
+        }
+    }
+}
---
+++ reflex-ldap/Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using reflex_ldap.Models;
+
+namespace reflex_ldap.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        { 
+            List<LDAPPacket> _list = new List<LDAPPacket>();
+            
+            _list.Add(new LDAPPacket(Name));
+            
+            InitializeComponent();
+        }
+    }
+}
